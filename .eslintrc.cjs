b3e313a5582ffd1a2753b7f5cc65ff3652aa6ccc
Jan Stupica <stupicajan@gmail.com>
2024-05-07 15:02:32 +0200

    Ran formatter and linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,39 +2,39 @@
 module.exports = {
 	root: true,
 	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
-		'prettier'
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:svelte/recommended",
+		"prettier",
 	],
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
+	parser: "@typescript-eslint/parser",
+	plugins: ["@typescript-eslint"],
 	parserOptions: {
-		sourceType: 'module',
+		sourceType: "module",
 		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
+		extraFileExtensions: [".svelte"],
 	},
 	env: {
 		browser: true,
 		es2017: true,
-		node: true
+		node: true,
 	},
 	rules: {
-		'no-shadow': 'off',
-		'@typescript-eslint/no-shadow': 'error',
-		'@typescript-eslint/no-var-requires': 'error',
-		'@typescript-eslint/no-empty-function': 'error',
-		'@typescript-eslint/explicit-function-return-type': 'error',
-		'@typescript-eslint/explicit-module-boundary-types': 'error',
-		'@typescript-eslint/no-explicit-any': 'error'
+		"no-shadow": "off",
+		"@typescript-eslint/no-shadow": "error",
+		"@typescript-eslint/no-var-requires": "error",
+		"@typescript-eslint/no-empty-function": "error",
+		"@typescript-eslint/explicit-function-return-type": "error",
+		"@typescript-eslint/explicit-module-boundary-types": "error",
+		"@typescript-eslint/no-explicit-any": "error",
 	},
 	overrides: [
 		{
-			files: ['*.svelte'],
-			parser: 'svelte-eslint-parser',
+			files: ["*.svelte"],
+			parser: "svelte-eslint-parser",
 			parserOptions: {
-				parser: '@typescript-eslint/parser'
-			}
-		}
-	]
+				parser: "@typescript-eslint/parser",
+			},
+		},
+	],
 };
--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,21 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({ 
+		adapter: adapter({
 			strict: false,
 			pages: "build",
 			assets: "build",
 			fallback: "index.html",
 		}),
 		paths: {
-            base: process.env.NODE_ENV === 'production' ? '' : '',
-        },
-	}
+			base: process.env.NODE_ENV === "production" ? "" : "",
+		},
+	},
 };
 
 export default config;
