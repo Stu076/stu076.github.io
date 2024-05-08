046ebb70d2710d3bf4dddd491906e06bedebbeb9
Jan Stupica <stupicajan@gmail.com>
2024-05-08 17:11:28 +0200

    first draft finished

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -10,24 +10,26 @@
 <header class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid " + (
     y > 0 ? " py-4 bg-slate-950 border-violet-950" : "py-6 bg-transparent border-transparent"
 )}>
-    <h1 class="font-medium text-6xl">
-        <p class="honk">Jan Stupica</p>
-        <p class="honk text-3xl">[Stu076]</p>
+    <h1 class="font-medium">
+        <p class="honk text-6xl sm:text-9xl">Jan Stupica</p>
+        <p class="honk text-3xl sm:text-6xl">[Stu076]</p>
     </h1>
-    <div class="sm:flex items-center gap-4 hidden">
+    <div class="sm:flex ml-auto pr-4 items-center gap-4 hidden">
         {#each tabs as tab, index}
             <a href={tab.link} class="duration-200 hover:text-violet-400">
                 <p>{tab.name}</p>
             </a>
         {/each}
-        <a href="https://www.linkedin.com/in/jan-stupica-153546178/"
-            target="_blank"
-            class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
-        >
-            <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200">
-
-            </div>
-            <h4 class="relative z-9 z-prds">Get in touch</h4>
-        </a>
+        
     </div>
+
+    <a href="https://www.linkedin.com/in/jan-stupica-153546178/"
+        target="_blank"
+        class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950"
+    >
+        <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200">
+
+        </div>
+        <h4 class="relative z-9">Get in touch</h4>
+    </a>
 </header>
